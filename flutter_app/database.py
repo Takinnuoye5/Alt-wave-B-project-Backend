dd9a4960c9afda06161e069bfda1bc40a73ec798
AKINNUOYE TIMOTHY <akinnuoyet@gmail.com>
2024-07-09 17:57:02 +0100

    Update configuration to use Heroku Postgres

--- flutter_app/database.py
+++ flutter_app/database.py
@@ -1,10 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./app.db"
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
@@ -19,7 +20,6 @@ def get_db():
 # Ensure all models are imported here so that they are registered correctly
 def init_db():
     import flutter_app.models  # Import models module, not specific models
-    
 
 # Call init_db() to initialize the database and create tables
 init_db()
--- migrations/env.py
+++ migrations/env.py
@@ -1,8 +1,6 @@
 from logging.config import fileConfig
-import sys
 import os
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
+from sqlalchemy import engine_from_config, pool
 from alembic import context
 
 # Import the Base from your application
@@ -16,6 +14,9 @@ config = context.config
 
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
+
+# Set the sqlalchemy.url value to the DATABASE_URL environment variable
+config.set_main_option('sqlalchemy.url', os.getenv('DATABASE_URL'))
 
 target_metadata = Base.metadata
 
@@ -33,15 +34,13 @@ def run_migrations_offline():
 
 def run_migrations_online():
     connectable = engine_from_config(
-        config.get_section(config.config_ini_section, {}),
+        config.get_section(config.config_ini_section),
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
 
     with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
+        context.configure(connection=connection, target_metadata=target_metadata)
 
         with context.begin_transaction():
             context.run_migrations()
