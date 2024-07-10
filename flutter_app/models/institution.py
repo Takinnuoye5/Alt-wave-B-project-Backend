b92719bf0faf63703f4d7ead3c04922df39fafe9
AKINNUOYE TIMOTHY <akinnuoyet@gmail.com>
2024-07-10 23:51:13 +0100

    fixes

--- flutter_app/models/institution.py
+++ flutter_app/models/institution.py
@@ -6,11 +6,11 @@ class Institution(Base):
     __tablename__ = "institutions"
 
     id = Column(Integer, primary_key=True, index=True)
-    schoolName = Column(String, index=True)
-    countryName = Column(String, index=True)  # Use country_name
+    school_name = Column(String, index=True)
+    country_name = Column(String, index=True)
     address = Column(String, index=True)
-    paymentType = Column(String, index=True)
-    contactEmail = Column(String, index=True)
+    payment_type = Column(String, index=True)
+    contact_email = Column(String, index=True)
     user_id = Column(Integer, ForeignKey('users.id'))
 
     user = relationship("User", back_populates="institutions")
--- flutter_app/routers/institution.py
+++ flutter_app/routers/institution.py
@@ -1,5 +1,3 @@
-
-# backend/routers/institution.py
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 import logging
@@ -14,7 +12,7 @@ logger = logging.getLogger(__name__)
 def create_institution(institution: schemas.InstitutionCreate, db: Session = Depends(get_db), current_user: models.User = Depends(get_current_user)):
     try:
         logger.info(f"Received data: {institution}")
-        created_institution = services.InstitutionService.create_institution(db, institution, user_id=current_user.id)  # Pass user_id here
+        created_institution = services.InstitutionService.create_institution(db, institution, current_user.id)
         logger.info(f"Created institution: {created_institution}")
         return created_institution
     except HTTPException as e:
@@ -26,12 +24,12 @@ def create_institution(institution: schemas.InstitutionCreate, db: Session = Dep
 
 @router.get("/institutions/", response_model=list[schemas.Institution])
 def read_institutions(skip: int = 0, limit: int = 10, db: Session = Depends(get_db), current_user: models.User = Depends(get_current_user)):
-    institutions = services.InstitutionService.get_institutions(db, user_id=current_user.id, skip=skip, limit=limit)  # Pass user_id here
+    institutions = services.InstitutionService.get_institutions(db, current_user.id, skip=skip, limit=limit)
     return institutions
 
 @router.get("/institutions/{institution_id}", response_model=schemas.Institution)
 def read_institution(institution_id: int, db: Session = Depends(get_db), current_user: models.User = Depends(get_current_user)):
-    db_institution = services.InstitutionService.get_institution(db, user_id=current_user.id, institution_id=institution_id)  # Pass user_id here
+    db_institution = services.InstitutionService.get_institution(db, current_user.id, institution_id)
     if db_institution is None:
         raise HTTPException(status_code=404, detail="Institution not found")
     return db_institution
