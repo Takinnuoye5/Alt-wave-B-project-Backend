c72ee057db7d63418a2d15cde305e8176e5aa0f9
AKINNUOYE TIMOTHY <akinnuoyet@gmail.com>
2024-08-16 10:14:32 +0100

    fix cors issues

--- main.py
+++ main.py
@@ -33,7 +33,10 @@ origins = [
     "http://localhost:3000",
     "http://localhost:3001",
     "http://localhost:4173",
-    "http://localhost:5173"
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "http://localhost:5175",
+    "http://localhost:5176",
     "https://altwave-rt2d.vercel.app"
 ]
 
